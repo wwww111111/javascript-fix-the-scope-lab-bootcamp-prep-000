9d0eac7715acadecb4a3cd0bf7c944f7a3615d9a
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 14:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  
-  return animal
+  let animal = "cat";
+  return animal;
 }
 
 function add2(n) {
